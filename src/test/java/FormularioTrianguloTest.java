a7b798531d41c21ea968b92dd67a3a6610f712b6
Graciela Gallas <graciela.carmen@gmail.com>
2018-06-24 20:22:04 -0300

    Ajustes webdriver e evidencias

--- src/test/java/FormularioTrianguloTest.java
+++ src/test/java/FormularioTrianguloTest.java
@@ -4,8 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import suporte.Screenshot;
+import utils.Screenshot;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
@@ -16,11 +18,15 @@ public class FormularioTrianguloTest {
     private static final String VINTE = "20";
     private static final String TRINTA = "30";
     private static final String ZERO = "0";
+
+    private static final String PATH_CHROMEDRIVER = ".\\webdriver\\chromedriver.exe";
+    private static final String PATH_EVIDENCIAS = ".\\evidencias\\";
+
     private WebDriver navegador;
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Graciela\\IdeaProjects\\Drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", PATH_CHROMEDRIVER);
         navegador = new ChromeDriver();
         navegador.manage().window().maximize();
         navegador.get("localhost:9090/#/");
@@ -37,7 +43,7 @@ public class FormularioTrianguloTest {
         WebElement resposta1 = navegador.findElement(By.xpath("//*[contains(text(), 'Equilátero')]"));
         String respostaAtual1 = resposta1.getText();
         assertEquals("Equilátero", respostaAtual1);
-        Screenshot.take(navegador, "C:\\Users\\Graciela\\IdeaProjects\\testePortoTech\\evidencias\\" + "teste_01.png");
+        Screenshot.take(navegador, PATH_EVIDENCIAS + "caso_teste_01.png");
         navegador.findElement(By.xpath("//button[@class=\"btn btn-danger\"]")).click();
 
         //Caso de teste 02
@@ -48,7 +54,7 @@ public class FormularioTrianguloTest {
         WebElement resposta2 = navegador.findElement(By.xpath("//*[contains(text(), 'Isóceles')]"));
         String respostaAtual2 = resposta2.getText();
         assertEquals("Isóceles", respostaAtual2);
-        Screenshot.take(navegador, "C:\\Users\\Graciela\\IdeaProjects\\testePortoTech\\evidencias\\" + "teste_02.png");
+        Screenshot.take(navegador, PATH_EVIDENCIAS + "caso_teste_02.png");
         navegador.findElement(By.xpath("//button[@class=\"btn btn-danger\"]")).click();
 
         //Caso de teste 03
@@ -59,7 +65,7 @@ public class FormularioTrianguloTest {
         WebElement resposta3 = navegador.findElement(By.xpath("//*[contains(text(), 'Escaleno')]"));
         String respostaAtual3 = resposta3.getText();
         assertEquals("Escaleno", respostaAtual3);
-        Screenshot.take(navegador, "C:\\Users\\Graciela\\IdeaProjects\\testePortoTech\\evidencias\\" + "teste_03.png");
+        Screenshot.take(navegador, PATH_EVIDENCIAS + "caso_teste_03.png");
         navegador.findElement(By.xpath("//button[@class=\"btn btn-danger\"]")).click();
         navegador.close();
     }
@@ -74,9 +80,14 @@ public class FormularioTrianguloTest {
         WebElement resposta4 = navegador.findElement(By.xpath("//div[@class='alert alert-danger fade show']"));
         String respostaAtual4 = resposta4.getText();
         assertEquals("Não é um triângulo", respostaAtual4);
-        Screenshot.take(navegador, "C:\\Users\\Graciela\\IdeaProjects\\testePortoTech\\evidencias\\" + "teste_04.png");
+        Screenshot.take(navegador, PATH_EVIDENCIAS + gerarTimestamp() + "caso_teste_04.png");
         navegador.findElement(By.xpath("//button[@class=\"btn btn-danger\"]")).click();
         navegador.close();
+    }
+
+    private static String gerarTimestamp() {
+        Timestamp ts = new Timestamp(System.currentTimeMillis());
+        return new SimpleDateFormat("yyyyMMddhhmmss").format(ts);
     }
 
 }
